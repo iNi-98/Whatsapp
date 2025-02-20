9cb33abeac77543ac307fd0204370477f7473cce
iNi-98 <inianj34@gmail.com>
2025-02-20 13:43:33 +0530

    Update Whatsapp_001.java

--- src/test/java/com/git/Whatsapp_001.java
+++ src/test/java/com/git/Whatsapp_001.java
@@ -1,1 +1,29 @@
-hjnbhmv
+package com.git;
+
+public class Whatsapp_001 {
+
+	private void m1() {
+		System.out.println("1");
+	}
+	private void m2() {
+		System.out.println("2");
+		
+	}
+	private void m3() {
+		System.out.println("3");
+		
+	}
+	private void m4() {
+		System.out.println("4");
+		
+	}
+	private void w1() {
+		System.out.println("fdf");
+	}
+	private void w2() {
+		System.out.println("re");
+	}
+	private void w3() {
+		System.out.println("sd");
+	}
+}
